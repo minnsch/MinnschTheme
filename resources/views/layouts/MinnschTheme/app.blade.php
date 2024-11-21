c409cbb01cd048cbee3387891c08f86ce362041d
Rendradinata Rafid <135959224+minnsch@users.noreply.github.com>
2024-11-21 21:00:33 +0900

    Fixed App

--- resources/views/layouts/MinnschTheme/app.blade.php
+++ resources/views/layouts/MinnschTheme/app.blade.php
@@ -20,8 +20,6 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
-  <link href="{{ public_mix('/assets/frontend/css/now-ui-kit.css') }}" rel="stylesheet"/>
-  <link href="{{ public_asset('/assets/frontend/css/styles.css') }}" rel="stylesheet"/>
   <link href="{{ public_asset('/minnsch/css/style.css')}}" rel="stylesheet"/>
   <link href="{{ public_mix('/assets/global/css/vendor.css') }}" rel="stylesheet"/>
  
